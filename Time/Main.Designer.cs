321ad653229a6175752a699430a08b84c8ee7228
RVladislav <Vladislav.Razhnov@gmail.com>
2015-02-25 01:27:25 +0300

    Style and located

---
+++ Time/Main.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Time
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.positionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.leftToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.centerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // lblTime
+            // 
+            resources.ApplyResources(this.lblTime, "lblTime");
+            this.lblTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime.ContextMenuStrip = this.contextMenuStrip;
+            this.lblTime.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblTime.Name = "lblTime";
+            this.lblTime.DoubleClick += new System.EventHandler(this.lblTime_DoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.positionToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            resources.ApplyResources(this.contextMenuStrip, "contextMenuStrip");
+            // 
+            // positionToolStripMenuItem
+            // 
+            this.positionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.leftToolStripMenuItem,
+            this.centerToolStripMenuItem,
+            this.rightToolStripMenuItem});
+            this.positionToolStripMenuItem.Name = "positionToolStripMenuItem";
+            resources.ApplyResources(this.positionToolStripMenuItem, "positionToolStripMenuItem");
+            // 
+            // leftToolStripMenuItem
+            // 
+            this.leftToolStripMenuItem.Name = "leftToolStripMenuItem";
+            resources.ApplyResources(this.leftToolStripMenuItem, "leftToolStripMenuItem");
+            this.leftToolStripMenuItem.Click += new System.EventHandler(this.leftToolStripMenuItem_Click);
+            // 
+            // centerToolStripMenuItem
+            // 
+            this.centerToolStripMenuItem.Name = "centerToolStripMenuItem";
+            resources.ApplyResources(this.centerToolStripMenuItem, "centerToolStripMenuItem");
+            this.centerToolStripMenuItem.Click += new System.EventHandler(this.centerToolStripMenuItem_Click);
+            // 
+            // rightToolStripMenuItem
+            // 
+            this.rightToolStripMenuItem.Name = "rightToolStripMenuItem";
+            resources.ApplyResources(this.rightToolStripMenuItem, "rightToolStripMenuItem");
+            this.rightToolStripMenuItem.Click += new System.EventHandler(this.rightToolStripMenuItem_Click);
+            // 
+            // Main
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ContextMenuStrip = this.contextMenuStrip;
+            this.Controls.Add(this.lblTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.DoubleClick += new System.EventHandler(this.Main_DoubleClick);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem positionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem leftToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem centerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rightToolStripMenuItem;
+    }
+}
+
---
+++ Time/Main.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Time
+{
+    public partial class Main : Form
+    {
+        string h, m, s;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            h = DateTime.Now.Hour.ToString();
+            m = DateTime.Now.Minute.ToString();
+            s = DateTime.Now.Second.ToString();
+
+            if (h.Count() < 2)
+            {
+                h = "0" + h;
+            }
+            if (m.Count() < 2)
+            {
+                m = "0" + m;
+            }
+            if (s.Count() < 2)
+            {
+                s = "0" + s;
+            }
+
+            lblTime.Text = h + ":" + m + ":" + s;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //прозрачное окно
+            //Color cl = lblTime.BackColor;
+            //this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            //this.BackColor = System.Drawing.Color.WhiteSmoke;
+            //this.TransparencyKey = System.Drawing.Color.WhiteSmoke;
+            //lblTime.BackColor = cl;
+            this.Opacity = 0.7;
+        }
+
+        private void leftToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Point pt = Screen.PrimaryScreen.WorkingArea.Location;
+            pt.Offset(0, 0);
+            this.Location = pt;
+        }
+
+        private void centerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Point pt = Screen.PrimaryScreen.WorkingArea.Location;
+            pt.Offset(Screen.PrimaryScreen.WorkingArea.Width/2 - this.Width/2, 0);
+            this.Location = pt;
+        }
+
+        private void rightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Point pt = Screen.PrimaryScreen.WorkingArea.Location;
+            pt.Offset(Screen.PrimaryScreen.WorkingArea.Width - this.Width, 0);
+            this.Location = pt;
+        }
+
+        private void lblTime_DoubleClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Main_DoubleClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
